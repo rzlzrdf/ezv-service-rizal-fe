aa051140437d7f9025f671d8457e8f2f0b136429
rzlzrdf <rizallazu@student.ub.ac.id>
2025-07-18 09:04:35 +0700

    fix quote

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -20,7 +20,7 @@ export default async function page({ params }: { params: { slug: string } }) {
         Back to Home
       </Link>
       <div className="text-2xl font-bold h-[90vh] bg-yellow-300 p-5 relative grid place-content-center">
-        <h1 className="text-3xl font-bold mb-4">"{note.title}"</h1>
+        <h1 className="text-3xl font-bold mb-4">{note.title}</h1>
         <div className="absolute bottom-0 right-0 p-3 bg-yellow-100">
           <p className="text-sm text-gray-600">
             Note Status: {note.completed ? "Completed" : "Undone"}
